ad7eafaadd6e297834506db16afd133cf1416d5e
Prabhashi Hettiarachchi <134066010+PrabhashiV@users.noreply.github.com>
2023-10-14 21:29:01 +0530

    changes

--- API_Automation_QATest/APITests.cs
+++ API_Automation_QATest/APITests.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+//using AutoFixture;
+//using AutoFixture.Xunit2;
+using System.Collections.Generic;
 
 namespace API_Automation_QATest
 {
@@ -25,15 +28,10 @@ namespace API_Automation_QATest
             try
             {
                 var response = await _client.GetAsync("objects");
-                response.EnsureSuccessStatusCode(); // Ensure HTTP status code is success (200).
-
-                // Add more assertions as needed for the response content.
-                // For example, check the response content or data format.
+                response.EnsureSuccessStatusCode();
             }
             catch (HttpRequestException ex)
             {
-                // Handle HTTP request exception (e.g., network error).
-                // You can log the error or fail the test.
                 Assert.True(false, ex.Message);
             }
         }
@@ -47,7 +45,7 @@ namespace API_Automation_QATest
                 int objectId2 = 5;
                 int objectId3 = 10;
 
-                var response = await _client.GetAsync($"objects/?id={objectId1}&id={objectId2}&id={objectId3}");
+                var response = await _client.GetAsync($"objects?id={objectId1}&id={objectId2}&id={objectId3}");
                 response.EnsureSuccessStatusCode();
 
             }
@@ -88,35 +86,8 @@ namespace API_Automation_QATest
                 var responseContent = await response.Content.ReadAsStringAsync();
                 var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
 
-                // Access the "id" property from the response object (it's a string).
                 NewObjectid = responseObject.id;
-            }
-            catch (HttpRequestException ex)
-            {
-                Assert.True(false, ex.Message);
-            }
-        }
 
-        [Fact]
-        public async Task Test_VerifyObjectCreated()
-        {
-            try
-            {
-                string objectId = NewObjectid;
-
-                var response = await _client.GetAsync($"objects/{objectId}");
-                response.EnsureSuccessStatusCode();
-
-                var responseContent = await response.Content.ReadAsStringAsync();
-                var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
-
-                string name = responseObject.name;
-                int year = responseObject.data.year;
-                decimal price = responseObject.data.price;
-                string cpumodel = responseObject.data.CPUModel;
-                string harddisksize = responseObject.data.HardDiskSize;
-
-                // Assert that the retrieved values match the expected values
                 Assert.Equal("Sample Name", responseObject.name);
                 Assert.Equal(2023, responseObject.data.year);
                 Assert.Equal(2000.00m, responseObject.data.price);
@@ -129,42 +100,21 @@ namespace API_Automation_QATest
             }
         }
 
+
         [Fact]
         public async Task Test_UpdateObject()
         {
             try
             {
-                string objectId = NewObjectid;
                 var json = "{\"name\":\"Sample Name EDITED\",\"data\":{\"year\":2024,\"price\":2001.00,\"CPU model\":\"Intel Core i10\",\"Hard disk size\":\"2 TB\"}}";
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                var response = await _client.PutAsync($"objects/{objectId}", content);
+                var response = await _client.PutAsync($"objects/{NewObjectid}", content);
                 response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException ex)
-            { 
-                Assert.True(false, ex.Message);
-            }
-        }
 
-        [Fact]
-        public async Task Test_VerifyObjectUpdated()
-        {
-            try
-            {
-                string objectId = NewObjectid;
-
-                var response = await _client.GetAsync($"objects/{objectId}");
-                response.EnsureSuccessStatusCode();
 
                 var responseContent = await response.Content.ReadAsStringAsync();
                 var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
-
-                string name = responseObject.name;
-                int year = responseObject.data.year;
-                decimal price = responseObject.data.price;
-                string cpumodel = responseObject.data.CPUModel;
-                string harddisksize = responseObject.data.HardDiskSize;
 
                 Assert.Equal("Sample Name EDITED", responseObject.name);
                 Assert.Equal(2024, responseObject.data.year);
@@ -173,7 +123,7 @@ namespace API_Automation_QATest
                 Assert.Equal("2 TB", responseObject.data.HardDiskSize);
             }
             catch (HttpRequestException ex)
-            {
+            { 
                 Assert.True(false, ex.Message);
             }
         }
@@ -183,37 +133,14 @@ namespace API_Automation_QATest
         {
             try
             {
-                string objectId = NewObjectid;
                 var json = "{\"name\": \"Sample Name EDITED AGAIN\"}";
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                var response = await _client.PutAsync($"objects/{objectId}", content);
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException ex)
-            {
-                Assert.True(false, ex.Message);
-            }
-        }
-
-        [Fact]
-        public async Task Test_VerifyObjectUpdatedPartially()
-        {
-            try
-            {
-                string objectId = NewObjectid;
-
-                var response = await _client.GetAsync($"objects/{objectId}");
+                var response = await _client.PutAsync($"objects/{NewObjectid}", content);
                 response.EnsureSuccessStatusCode();
 
                 var responseContent = await response.Content.ReadAsStringAsync();
                 var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
-
-                string name = responseObject.name;
-                int year = responseObject.data.year;
-                decimal price = responseObject.data.price;
-                string cpumodel = responseObject.data.CPUModel;
-                string harddisksize = responseObject.data.HardDiskSize;
 
                 Assert.Equal("Sample Name EDITED AGAIN", responseObject.name);
                 Assert.Equal(2024, responseObject.data.year);
@@ -232,25 +159,15 @@ namespace API_Automation_QATest
         {
             try
             {
-                string objectId = NewObjectid;
-
-                var response = await _client.DeleteAsync($"objects/{objectId}");
+                var response = await _client.DeleteAsync($"objects/{NewObjectid}");
                 response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException ex)
-            {
-                Assert.True(false, ex.Message);
-            }
-        }
-        [Fact]
-        public async Task Test_VerifyObjectDeleted()
-        {
-            try
-            {
-                string objectId = NewObjectid;
 
-                var response = await _client.GetAsync($"objects/{objectId}");
-                response.EnsureSuccessStatusCode();
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
+
+                string message = responseObject.message;
+
+                Assert.Equal($"Object with id = {NewObjectid} has been deleted.", responseObject.message);
             }
             catch (HttpRequestException ex)
             {
--- API_Automation_QATest/ApiResponse.cs
+++ API_Automation_QATest/ApiResponse.cs
@@ -1,17 +1,24 @@
-﻿namespace API_Automation_QATest
+﻿using Newtonsoft.Json;
+
+namespace API_Automation_QATest
 { 
     public class ApiResponse
 {
     public string id { get; set; }
     public string name { get; set; }
     public DataModel data { get; set; }
+
+    public string message { get; set; }
 }
 
 public class DataModel
 {
     public int year { get; set; }
     public decimal price { get; set; }
-    public string CPUModel { get; set; }
-    public string HardDiskSize { get; set; }
-}
+        [JsonProperty("CPU model")]
+        public string CPUModel { get; set; }
+
+        [JsonProperty("Hard disk size")]
+        public string HardDiskSize { get; set; }
+    }
 }
