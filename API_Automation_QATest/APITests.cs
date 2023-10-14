332881ad37ef98ee7edbca19528f5256907b489e
Prabhashi Hettiarachchi <134066010+PrabhashiV@users.noreply.github.com>
2023-10-14 09:12:57 +0530

    Added additional verification points

--- API_Automation_QATest/APITests.cs
+++ API_Automation_QATest/APITests.cs
@@ -11,133 +11,251 @@ namespace API_Automation_QATest
     public class APITests
     {
         private readonly HttpClient _client;
-        private int NewObjectid;
+        private string NewObjectid;
 
         public APITests()
         {
             _client = new HttpClient();
-            _client.BaseAddress = new Uri("https://restful-api.dev/");
+            _client.BaseAddress = new Uri("https://api.restful-api.dev/");
         }
 
         [Fact]
         public async Task Test_GetAllObjects()
         {
-            // Arrange
+            try
+            {
+                var response = await _client.GetAsync("objects");
+                response.EnsureSuccessStatusCode(); // Ensure HTTP status code is success (200).
 
-            // Act
-            var response = await _client.GetAsync("objects");
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // Add more assertions as needed for the response content.
-            Console.WriteLine("Test Executed");
+                // Add more assertions as needed for the response content.
+                // For example, check the response content or data format.
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle HTTP request exception (e.g., network error).
+                // You can log the error or fail the test.
+                Assert.True(false, ex.Message);
+            }
         }
 
         [Fact]
         public async Task Test_GetListofObjectsByIds()
         {
-            // Arrange
-            int objectId1 = 3;
-            int objectId2 = 5;
-            int objectId3 = 10;
+            try
+            {
+                int objectId1 = 3;
+                int objectId2 = 5;
+                int objectId3 = 10;
 
-            // Act
-            var response = await _client.GetAsync($"objects/?id={objectId1}&id={objectId2}&id={objectId3}");
+                var response = await _client.GetAsync($"objects/?id={objectId1}&id={objectId2}&id={objectId3}");
+                response.EnsureSuccessStatusCode();
 
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // Add more assertions as needed for the response content.
-            Console.WriteLine("Test Executed");
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
         [Fact]
         public async Task Test_GetSingleObjectById()
         {
-            // Arrange
-            int objectId = 7;
+            try
+            {
+                int objectId = 7;
 
-            // Act
-            var response = await _client.GetAsync($"objects/{objectId}");
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // Add more assertions as needed for the response content.
-            Console.WriteLine("Test Executed");
+                var response = await _client.GetAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
+
         [Fact]
-        public async Task Test_AddObjectUsingPost()
+        public async Task Test_AddObject()
         {
-            // Arrange
-            var jsonData = "{\"name\":\"Sample Name\",\"data\":{\"year\":2023,\"price\":2000.00,\"CPU model\":\"Intel Core i9\",\"Hard disk size\":\"1 TB\"}}";
-            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            // Act
-            var response = await _client.PostAsync("objects", content);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-            // Add more assertions as needed for the response content.
-
-            if (response.StatusCode == HttpStatusCode.Created)
+            try
             {
-                // Parse the response content to extract the ID.
+                var jsonData = "{\"name\":\"Sample Name\",\"data\":{\"year\":2023,\"price\":2000.00,\"CPU model\":\"Intel Core i9\",\"Hard disk size\":\"1 TB\"}}";
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                var response = await _client.PostAsync("objects", content);
+                response.EnsureSuccessStatusCode();
+
                 var responseContent = await response.Content.ReadAsStringAsync();
                 var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
 
-                // Access the ID property from the response object.
-                NewObjectid = responseObject.Id;
-
+                // Access the "id" property from the response object (it's a string).
+                NewObjectid = responseObject.id;
             }
-            Console.WriteLine("Test Executed");
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+        }
+
+        [Fact]
+        public async Task Test_VerifyObjectCreated()
+        {
+            try
+            {
+                string objectId = NewObjectid;
+
+                var response = await _client.GetAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
+
+                string name = responseObject.name;
+                int year = responseObject.data.year;
+                decimal price = responseObject.data.price;
+                string cpumodel = responseObject.data.CPUModel;
+                string harddisksize = responseObject.data.HardDiskSize;
+
+                // Assert that the retrieved values match the expected values
+                Assert.Equal("Sample Name", responseObject.name);
+                Assert.Equal(2023, responseObject.data.year);
+                Assert.Equal(2000.00m, responseObject.data.price);
+                Assert.Equal("Intel Core i9", responseObject.data.CPUModel);
+                Assert.Equal("1 TB", responseObject.data.HardDiskSize);
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
         [Fact]
         public async Task Test_UpdateObject()
         {
-            // Arrange
-            int objectId = NewObjectid;
-            var json = "{\"name\":\"Sample Name EDITED\",\"data\":{\"year\":2024,\"price\":2001.00,\"CPU model\":\"Intel Core i10\",\"Hard disk size\":\"2 TB\"}}";
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                string objectId = NewObjectid;
+                var json = "{\"name\":\"Sample Name EDITED\",\"data\":{\"year\":2024,\"price\":2001.00,\"CPU model\":\"Intel Core i10\",\"Hard disk size\":\"2 TB\"}}";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            // Act
-            var response = await _client.PutAsync($"objects/{objectId}", content);
+                var response = await _client.PutAsync($"objects/{objectId}", content);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            { 
+                Assert.True(false, ex.Message);
+            }
+        }
 
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // Add more assertions as needed for the response content.
-            Console.WriteLine("Test Executed");
+        [Fact]
+        public async Task Test_VerifyObjectUpdated()
+        {
+            try
+            {
+                string objectId = NewObjectid;
+
+                var response = await _client.GetAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
+
+                string name = responseObject.name;
+                int year = responseObject.data.year;
+                decimal price = responseObject.data.price;
+                string cpumodel = responseObject.data.CPUModel;
+                string harddisksize = responseObject.data.HardDiskSize;
+
+                Assert.Equal("Sample Name EDITED", responseObject.name);
+                Assert.Equal(2024, responseObject.data.year);
+                Assert.Equal(2001.00m, responseObject.data.price);
+                Assert.Equal("Intel Core i10", responseObject.data.CPUModel);
+                Assert.Equal("2 TB", responseObject.data.HardDiskSize);
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
         [Fact]
         public async Task Test_UpdateObjectPartially()
         {
-            // Arrange
-            int objectId = NewObjectid;
-            var json = "{\"name\": \"Sample Name EDITED AGAIN\"";
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                string objectId = NewObjectid;
+                var json = "{\"name\": \"Sample Name EDITED AGAIN\"}";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            // Act
-            var response = await _client.PutAsync($"objects/{objectId}", content);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // Add more assertions as needed for the response content.
-            Console.WriteLine("Test Executed");
+                var response = await _client.PutAsync($"objects/{objectId}", content);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
         [Fact]
-        public async Task Test_DeleteObjectUsingDelete()
+        public async Task Test_VerifyObjectUpdatedPartially()
         {
-            // Arrange
-            int objectId = NewObjectid;
+            try
+            {
+                string objectId = NewObjectid;
 
-            // Act
-            var response = await _client.DeleteAsync($"objects/{objectId}");
+                var response = await _client.GetAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
 
-            // Assert
-            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-            Console.WriteLine("Test Executed");
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
+
+                string name = responseObject.name;
+                int year = responseObject.data.year;
+                decimal price = responseObject.data.price;
+                string cpumodel = responseObject.data.CPUModel;
+                string harddisksize = responseObject.data.HardDiskSize;
+
+                Assert.Equal("Sample Name EDITED AGAIN", responseObject.name);
+                Assert.Equal(2024, responseObject.data.year);
+                Assert.Equal(2001.00m, responseObject.data.price);
+                Assert.Equal("Intel Core i10", responseObject.data.CPUModel);
+                Assert.Equal("2 TB", responseObject.data.HardDiskSize);
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+        }
+
+        [Fact]
+        public async Task Test_DeleteObject()
+        {
+            try
+            {
+                string objectId = NewObjectid;
+
+                var response = await _client.DeleteAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+        }
+        [Fact]
+        public async Task Test_VerifyObjectDeleted()
+        {
+            try
+            {
+                string objectId = NewObjectid;
+
+                var response = await _client.GetAsync($"objects/{objectId}");
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
     }
 }
--- API_Automation_QATest/ApiResponse.cs
+++ API_Automation_QATest/ApiResponse.cs
@@ -2,15 +2,15 @@
 { 
     public class ApiResponse
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DataModel Data { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+    public DataModel data { get; set; }
 }
 
 public class DataModel
 {
-    public int Year { get; set; }
-    public decimal Price { get; set; }
+    public int year { get; set; }
+    public decimal price { get; set; }
     public string CPUModel { get; set; }
     public string HardDiskSize { get; set; }
 }
